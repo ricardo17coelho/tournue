41790b43c2e037b588c8b7aba0d27db747fac514
Ricardo Coelho <devel@rmorgado.ch>
2023-11-05 17:53:50 +0100

    chore: add vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue"
+import router from "@/router"
+import vuetify from "@/plugins/vuetify"
 import './assets/main.css'
 
-import vuetify from './plugins/vuetify'
 
 const app = createApp(App)
 
